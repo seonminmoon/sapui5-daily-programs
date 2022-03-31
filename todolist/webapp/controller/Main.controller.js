f449ea2d9c5f031a553a871ea18cf65fd087cdc2
문선민 <seonminmoon@gmail.com>
2022-03-31 22:30:45 +0900

    table add, delete event setting

--- todolist/webapp/controller/Main.controller.js
+++ todolist/webapp/controller/Main.controller.js
@@ -1,15 +1,27 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+	['sap/ui/core/mvc/Controller'],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller) {
+		'use strict';
 
-        return Controller.extend("todolist.controller.Main", {
-            onInit: function () {
+		return Controller.extend('todolist.controller.Main', {
+			onInit: function () {
+				let oModel = this.getOwnerComponent().getModel();
+				oModel.setData({
+					'uiTable': [],
+					'mTable': [],
+				});
+			},
 
-            }
-        });
-    });
+			onAdd1: function () {},
+
+			onDelete1: function () {},
+
+			onAdd2: function () {},
+
+			onDelete2: function () {},
+		});
+	}
+);
