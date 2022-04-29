e58233d33014fe7c33a1bd709623dd56ad153617
문선민 <seonminmoon@gmail.com>
2022-04-29 22:29:22 +0900

    불필요한 코드 정리

--- processflow/webapp/Component.js
+++ processflow/webapp/Component.js
@@ -1,31 +1,22 @@
-sap.ui.define([
-        "sap/ui/core/UIComponent",
-        "sap/ui/Device",
-        "processflow/model/models"
-    ],
-    function (UIComponent, Device, models) {
-        "use strict";
+sap.ui.define(['sap/ui/core/UIComponent', 'sap/ui/Device'], function (UIComponent, Device) {
+	'use strict';
 
-        return UIComponent.extend("processflow.Component", {
-            metadata: {
-                manifest: "json"
-            },
+	return UIComponent.extend('processflow.Component', {
+		metadata: {
+			manifest: 'json',
+		},
 
-            /**
-             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-             * @public
-             * @override
-             */
-            init: function () {
-                // call the base component's init function
-                UIComponent.prototype.init.apply(this, arguments);
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * @public
+		 * @override
+		 */
+		init: function () {
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
 
-                // enable routing
-                this.getRouter().initialize();
-
-                // set the device model
-                this.setModel(models.createDeviceModel(), "device");
-            }
-        });
-    }
-);
+			// enable routing
+			this.getRouter().initialize();
+		},
+	});
+});
